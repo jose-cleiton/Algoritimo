79a53c7f880d732e9ea4ed68f137ebf44ae78341
jose-cleiton <cleitons835@gmail.com>
2022-11-28 16:03:10 -0300

    feat: :sparkles: Metodo insert no indice escolhido

--- listas_encadeadas/linked_list.py
+++ listas_encadeadas/linked_list.py
@@ -23,31 +23,28 @@ class Linkedilist:
     def __len__(self):
         return self._size
 
-    def get(self, index):
-        pass
-
-    def set(self, index, elem):
-        pass
-
-    def __getitem__(self, index):
+    def _getnode(self, index):
         pointer = self.head
         for i in range(index):
             if pointer:
                 pointer = pointer.next
             else:
                 raise IndexError("<-list index out of range->")
+
+        return pointer
+
+    def set(self, index, elem):
+        pass
+
+    def __getitem__(self, index):
+        pointer = self._getnode(index)
         if pointer:
             return pointer.data
 
         raise IndexError("<-list index out of range->")
 
     def __setitem__(self, index, elem):
-        pointer = self.head
-        for i in range(index):
-            if pointer:
-                pointer = pointer.next
-            else:
-                raise IndexError("<-list index out of range->")
+        pointer = self._getnode(index)
         if pointer:
             pointer.data = elem
         else:
@@ -63,6 +60,17 @@ class Linkedilist:
             i = i + 1
         raise ValueError("{} not in list->".format(elem))
 
+    def insert(self, index, elem):
+        if index == 0:
+            node = Node(elem)
+            node.next = self.head
+            self.head = node
+        else:
+            pointer = self._getnode(index - 1)
+            node.next = pointer.next
+            pointer.next = node
+        self._size = self._size + 1
+
 
 if __name__ == "__main__":
     os.system("cls" if os.name == "nt" else "clear")
@@ -72,4 +80,7 @@ if __name__ == "__main__":
     lista.append(1)
     lista.append(2)
     print(lista.index(2))
-   
+    print(len(lista))
+    lista.insert(0, 3)
+    print(lista.index(3))
+    print(lista[0])
